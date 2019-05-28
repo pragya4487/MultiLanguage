b4664a55692c67b4f4a5c72f87c13bea7ca6eaae
pragya4487 <pragyajain.jp@gmail.com>
2019-05-28 10:42:06 +0530

    Login/Logout

---
+++ app/src/main/java/com/example/multilanguage/SecondActivity.java
@@ -0,0 +1,31 @@
+package com.example.multilanguage;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+    Button btnlogout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        btnlogout=findViewById(R.id.btnlogout);
+
+        btnlogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+               Intent intent=new Intent();
+                setResult(2,intent);
+                //finish();
+            }
+        });
+
+    }
+}
